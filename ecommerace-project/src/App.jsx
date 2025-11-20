5ae479c7e5ae35553dea0a694d3c93c35fc9099c
umar5252-g <mromertariq@gmail.com>
2025-11-20 21:55:50 +0500

    move checkout page into react + update all the links to match the router

--- ecommerace-project/src/App.jsx
+++ ecommerace-project/src/App.jsx
@@ -1,13 +1,18 @@
 import {HomePage} from "./Pages/HomePage"
+import { CheckoutPage } from "./Pages/CheckoutPage"
+import { Routes , Route} from "react-router"
+
 import './App.css'
 
 function App() {
-
-
   return (
     <>
-<HomePage />
-  
+    <Routes>
+      <Route path="/" element = {<HomePage />} />
+      <Route path="/checkout" element = {<CheckoutPage/>}/>
+      
+    </Routes>
+
     </>
   )
 }
