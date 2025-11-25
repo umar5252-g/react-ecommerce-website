eeaa9dfb9b0e9959db2e2235f049b3d5f0a200b7
umar5252-g <mromertariq@gmail.com>
2025-11-25 21:38:06 +0500

    fetch data from the backend

--- ecommerce-project/src/Pages/HomePage.jsx
+++ ecommerce-project/src/Pages/HomePage.jsx
@@ -2,6 +2,13 @@ import "./HomePage.css";
 import { Header } from "../components/Header";
 import { products } from "../../starting-code/data/products";
 export function HomePage() {
+  fetch("http://localhost:3000/api/products")
+    .then((response) => {
+      return response.json();
+    })
+    .then((products) => {
+      console.log(products);
+    });
   return (
     <>
       <title>Ecommerce Project</title>
