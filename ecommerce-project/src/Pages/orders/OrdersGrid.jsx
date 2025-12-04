40f1875092299b4ceb7e7eface13efb10aba549c
umar5252-g <mromertariq@gmail.com>
2025-12-04 13:49:55 +0500

    fix a bug and separate the orderdetails grid

--- ecommerce-project/src/Pages/orders/OrdersGrid.jsx
+++ ecommerce-project/src/Pages/orders/OrdersGrid.jsx
@@ -1,45 +1,33 @@
 import dayjs from "dayjs";
-import { Fragment } from "react";
-import { Link } from "react-router";
-
-export function OrdersGrid({ order }) {
+import { formatMoney } from "../../utils/money";
+import { OrderDetailsGrid } from "./OrderDetailsGrid";
+export function OrdersGrid({ orders }) {
   return (
-    <div className="order-details-grid">
-      {order.products.map((orderProduct) => {
-        return (
-          <Fragment key={orderProduct.productId}>
-            <div className="product-image-container">
-              <img src={orderProduct.product.image} />
-            </div>
-
-            <div className="product-details">
-              <div className="product-name">{orderProduct.product.name}</div>
-              <div className="product-delivery-date">
-                Arriving on:{" "}
-                {dayjs(orderProduct.estimatedDeliveryTimeMs).format("MMMM D")}
+    <div className="orders-grid">
+      {orders &&
+        orders.map((order) => {
+          return (
+            <div key={order.id} className="order-container">
+              <div className="order-header">
+                <div className="order-header-left-section">
+                  <div className="order-date">
+                    <div className="order-header-label">Order Placed:</div>
+                    <div>{dayjs(order.orderTimeMs).format("MMMM D")}</div>
+                  </div>
+                  <div className="order-total">
+                    <div className="order-header-label">Total:</div>
+                    <div>{formatMoney(order.totalCostCents)}</div>
+                  </div>
+                </div>
+                <div className="order-header-right-section">
+                  <div className="order-header-label">Order ID:</div>
+                  <div>{order.id}</div>
+                </div>
               </div>
-              <div className="product-quantity">
-                Quantity: {orderProduct.quantity}
-              </div>
-              <button className="buy-again-button button-primary">
-                <img
-                  className="buy-again-icon"
-                  src="images/icons/buy-again.png"
-                />
-                <span className="buy-again-message">Add to Cart</span>
-              </button>
+              <OrderDetailsGrid order={order} />
             </div>
-
-            <div className="product-actions">
-              <Link to="/tracking">
-                <button className="track-package-button button-secondary">
-                  Track package
-                </button>
-              </Link>
-            </div>
-          </Fragment>
-        );
-      })}
+          );
+        })}
     </div>
   );
 }
--- ecommerce-project/src/Pages/orders/OrdersPage.jsx
+++ ecommerce-project/src/Pages/orders/OrdersPage.jsx
@@ -1,8 +1,6 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-import dayjs from "dayjs";
 import { Link } from "react-router";
-import { formatMoney } from "../../utils/money";
 
 import "./orderspage.css";
 import { Header } from "../../components/Header";
@@ -58,33 +56,7 @@ export function OrdersPage({ cart }) {
       <div className="orders-page">
         <div className="page-title">Your Orders</div>
 
-        <div className="orders-grid">
-          {orders &&
-            orders.map((order) => {
-              return (
-                <div key={order.id} className="order-container">
-                  <div className="order-header">
-                    <div className="order-header-left-section">
-                      <div className="order-date">
-                        <div className="order-header-label">Order Placed:</div>
-                        <div>{dayjs(order.orderTimeMs).format("MMMM D")}</div>
-                      </div>
-                      <div className="order-total">
-                        <div className="order-header-label">Total:</div>
-                        <div>{formatMoney(order.totalCostCents)}</div>
-                      </div>
-                    </div>
-
-                    <div className="order-header-right-section">
-                      <div className="order-header-label">Order ID:</div>
-                      <div>{order.id}</div>
-                    </div>
-                  </div>
-                  <OrdersGrid order={order} />
-                </div>
-              );
-            })}
-        </div>
+        <OrdersGrid orders={orders} />
       </div>
     </>
   );
