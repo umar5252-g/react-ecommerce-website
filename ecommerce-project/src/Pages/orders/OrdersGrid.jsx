e47cd3397088b2ed8e62e82014095866da2fdabc
umar5252-g <mromertariq@gmail.com>
2025-12-04 13:55:52 +0500

    separate the orders header into its own component

--- ecommerce-project/src/Pages/orders/OrdersGrid.jsx
+++ ecommerce-project/src/Pages/orders/OrdersGrid.jsx
@@ -1,6 +1,5 @@
-import dayjs from "dayjs";
-import { formatMoney } from "../../utils/money";
 import { OrderDetailsGrid } from "./OrderDetailsGrid";
+import { OrdersHeader } from "./OrdersHeader";
 export function OrdersGrid({ orders }) {
   return (
     <div className="orders-grid">
@@ -8,22 +7,7 @@ export function OrdersGrid({ orders }) {
         orders.map((order) => {
           return (
             <div key={order.id} className="order-container">
-              <div className="order-header">
-                <div className="order-header-left-section">
-                  <div className="order-date">
-                    <div className="order-header-label">Order Placed:</div>
-                    <div>{dayjs(order.orderTimeMs).format("MMMM D")}</div>
-                  </div>
-                  <div className="order-total">
-                    <div className="order-header-label">Total:</div>
-                    <div>{formatMoney(order.totalCostCents)}</div>
-                  </div>
-                </div>
-                <div className="order-header-right-section">
-                  <div className="order-header-label">Order ID:</div>
-                  <div>{order.id}</div>
-                </div>
-              </div>
+              <OrdersHeader order={order} />
               <OrderDetailsGrid order={order} />
             </div>
           );
