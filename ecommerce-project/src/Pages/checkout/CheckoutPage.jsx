808ec5efaa5dfda45814fa923cc7158cf4b8559f
umar5252-g <mromertariq@gmail.com>
2025-12-10 15:42:47 +0500

    make placeOrder button work

--- ecommerce-project/src/Pages/checkout/CheckoutPage.jsx
+++ ecommerce-project/src/Pages/checkout/CheckoutPage.jsx
@@ -42,7 +42,7 @@ export function CheckoutPage({ cart, loadCart }) {
             deliveryOptions={deliveryOptions}
             loadCart={loadCart}
           />
-          <PaymentSummary paymentSummary={paymentSummary} />
+          <PaymentSummary paymentSummary={paymentSummary} loadCart={loadCart} />
         </div>
       </div>
     </>
--- ecommerce-project/src/Pages/orders/OrdersPage.jsx
+++ ecommerce-project/src/Pages/orders/OrdersPage.jsx
@@ -24,35 +24,6 @@ export function OrdersPage({ cart }) {
 
       <link rel="icon" type="image/svg+xml" href="orders-favicon.png" />
 
-      <div className="header">
-        <div className="left-section">
-          <Link to="/" className="header-link">
-            <img className="logo" src="images/logo-white.png" />
-            <img className="mobile-logo" src="images/mobile-logo-white.png" />
-          </Link>
-        </div>
-
-        <div className="middle-section">
-          <input className="search-bar" type="text" placeholder="Search" />
-
-          <button className="search-button">
-            <img className="search-icon" src="images/icons/search-icon.png" />
-          </button>
-        </div>
-
-        <div className="right-section">
-          <Link className="orders-link header-link" to="/orders">
-            <span className="orders-text">Orders</span>
-          </Link>
-
-          <Link className="cart-link header-link" to="/checkout">
-            <img className="cart-icon" src="images/icons/cart-icon.png" />
-            <div className="cart-quantity">3</div>
-            <div className="cart-text">Cart</div>
-          </Link>
-        </div>
-      </div>
-
       <div className="orders-page">
         <div className="page-title">Your Orders</div>
 
