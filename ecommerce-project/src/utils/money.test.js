cfa0f981e2d933c03174990b7231d344e895ad9b
umar5252-g <mromertariq@gmail.com>
2025-12-17 08:27:12 +0500

    add a test for -ive amount

--- ecommerce-project/src/utils/money.test.js
+++ ecommerce-project/src/utils/money.test.js
@@ -12,7 +12,11 @@ describe("formateMoney ", () => {
 
     expect(formatMoney(100)).toBe("$1.00");
   });
-  it("display $0.00", () => {
+  it("display $0.00 ", () => {
     expect(formatMoney(0)).toBe("$0.00");
+  });
+  it("display  negaitive value (-$9.99) ", () => {
+    expect(formatMoney(0)).toBe("$0.00");
+    expect(formatMoney(-999)).toBe("-$9.99");
   });
 });
